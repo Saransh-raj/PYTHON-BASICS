0d7485dfbee6b3ce75ff5e89b1789348c11e5476
Saransh-raj <saranshraj011@gmail.com>
2026-07-20 23:36:19 +0530

    type conversion in python

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ print(type(name)) # <class 'str'>
 print(type(age)) # <class 'int'>
 print(type(PI)) # <class 'float'>
 
+# sum of two numbers
+a = 10
+b = 15
+c = a+b
+print(c)
+
+# type conversion
+# String to Integer
+s = "100"
+a = int(s)
+print(a, type(a))   # 100 <class 'int'>
+
+# Number to String
+num = 42
+label = str(num)
+print(label, type(label))   # "42" <class 'str'>
+
