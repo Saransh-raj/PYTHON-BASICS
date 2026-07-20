2146bff6b4391cfefa8d5c6925d4601179317fd1
Saransh-raj <saranshraj011@gmail.com>
2026-07-21 00:28:11 +0530

    Input function in python

---
+++ input.py
@@ -0,0 +1,13 @@
+a = input("Enter value of a : ")
+print(a)
+
+num1 = int(input("Enter num 1 : "))
+num2 = int(input("Enter num 2 : "))
+result = num1+num2
+print(result)
+
+# WAP to taake the user input and print the average of 2 number
+num3 = int(input("Enter num 1 : "))
+num4 = int(input("Enter num 2 : "))
+average = (num3+num4) / 2
+print("Average : ",average)
