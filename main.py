bf3f9318628f97b3b1b706b4b56f085d11d5bc7d
Saransh-raj <saranshraj011@gmail.com>
2026-07-20 23:03:11 +0530

    variable and types

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Hello....!")
+print("Saransh \nRaj")
+
+name = "Saransh"
+age = 20
+PI = 3.14
+
+# printing the type of variable
+print(type(name)) # <class 'str'>
+print(type(age)) # <class 'int'>
+print(type(PI)) # <class 'float'>
+
