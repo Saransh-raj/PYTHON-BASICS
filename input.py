50b0b678e49745f74c1cfa4753916b00e5e069f7
Saransh-raj <saranshraj011@gmail.com>
2026-07-21 23:57:26 +0530

    Input question practice

--- input.py
+++ input.py
@@ -11,3 +11,17 @@ num3 = int(input("Enter num 1 : "))
 num4 = int(input("Enter num 2 : "))
 average = (num3+num4) / 2
 print("Average : ",average)
+
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+print("Name : ",name," age : ",age)
+
+# f-String
+print(f"Name : {name} and my age : {age}")
+
+# take cont,name,age from user and print it
+user_name = input("Enter your name : ")
+user_age = input("Enter age ")
+user_cont = input("Enter contact : ")
+
+print(f"NAME : {user_name} AGE : {user_age} CONTACT : {user_cont}")
