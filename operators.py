0ef81cfeac44bef44f3220e680d1e5ea80352e39
Saransh-raj <saranshraj011@gmail.com>
2026-07-22 00:23:09 +0530

    Operators in python

---
+++ operators.py
@@ -0,0 +1,38 @@
+# operators - they are symbols that performs operations on variable and values . python have several type of operators for diff tasks
+# 1. Arithematic operator - (+,-,*,/,//,%,**)
+
+a = 10
+b = 5
+print(a+b)  # 15
+print(a-b)  # 5
+print(a*b)  # 50
+print(a/b)  # 2.0
+print(a//b) # 2 called as floor division
+print(a%b)  # 0
+print(a**b) # 100000
+
+# 2. Assignment operators - (+=,-=,*=,/=,//=,%=,**=)
+x = 10
+
+x+=x
+print(x)
+
+x*=x
+print(x)
+
+# 3. Comparison Operators - (==,!=,<,>,<=,>=)
+m = 10
+n = 20
+print(m>n) # false
+print(n>m) # true
+print(m==n) # false
+print(m!=n) # true
+
+# 4. Logical Operators - (and , or , not)
+
+d = True
+f = False
+print(d and f)  # False
+print(d or f)   # True
+print(not d)    # False
+
