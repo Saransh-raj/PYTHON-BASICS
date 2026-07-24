6416823bae4410cd8ea9d357e9621051f9d9c9b0
Saransh-raj <saranshraj011@gmail.com>
2026-07-25 01:07:50 +0530

    function for checking string is palindrome or not

--- function.py
+++ function.py
@@ -17,3 +17,30 @@ def greeting(name):
 
 greeting("Saransh")
 
+# sum function with parameter 
+def sum(a,b):
+    print(f"The sum of your number is {a+b}")
+    
+sum(2,5)
+sum(12,10)
+
+# default argument function
+def sum2(x,y=42):
+    print(f"The sum of your number is {x+y}")
+
+sum2(10)
+
+# function for checking the string is palindrome or not
+def palindrome(str):
+    rev = ""
+    for i in range(len(str)-1,-1,-1):
+        rev = rev + str[i]
+        
+    if rev == str:
+        print("Palindrome")
+            
+    else:
+        print("Not a palindrome")
+            
+palindrome("Saransh")
+palindrome("KANAK")
