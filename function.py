84c5548ec766ad02fa1d729e216a6c8e442c6708
Saransh-raj <saranshraj011@gmail.com>
2026-07-24 23:46:35 +0530

    Function in python

---
+++ function.py
@@ -0,0 +1,18 @@
+# function
+def greet():
+    print("Hello Saransh")
+
+greet()
+
+# function without parameter 
+def welcome():
+    print("Welcome to python")
+    
+welcome()
+
+# function with parameter
+def greeting(name):
+    print("Hello ,",name)
+
+greeting("Saransh")
+
