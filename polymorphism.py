cb71d686e4cec0bd107ca227670d3e46ccaee3e0
Saransh-raj <saranshraj011@gmail.com>
2026-07-29 17:16:09 +0530

    Duck typing in python

--- polymorphism.py
+++ polymorphism.py
@@ -24,3 +24,54 @@ cat = Cat()
 dog.sound()
 cat.sound()
 
+# Duck typing in python :-
+
+# Example 1 :-
+
+class Bike:
+
+    def start(self):
+        print("Bike Started")
+
+
+class Car:
+
+    def start(self):
+        print("Car Started")
+
+
+class Bus:
+
+    def start(self):
+        print("Bus Started")
+
+# duck typing function
+def vehicle_start(vehicle):
+    vehicle.start()
+
+
+vehicle_start(Bike())
+vehicle_start(Car())
+vehicle_start(Bus())
+
+
+# Example 2 :-
+
+class Car:
+
+    def sound(self):
+        print("Horn")
+
+
+class Dog:
+
+    def sound(self):
+        print("Bark")
+
+# duck typing function
+def play(obj):
+    obj.sound()
+
+
+play(Car())
+play(Dog())
